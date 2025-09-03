7df476e07101051c59f1dd57c8f26843064d3041
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-09-03 10:22:32 +0530

    LAB PROGRAM 7

---
+++ DAY PROGRAMS/DAY9.js
@@ -0,0 +1,7 @@
+// import and export modules
+
+// definition of a module
+// export allows you to make variables and functions available to other modules
+// classes available to other files
+// import allow you to bring those into another file
+// used in modular javascript (ES6+).
